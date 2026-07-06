62bb3d75473c6492a40a1804695a530053c2c697
Miguel23lp <miguel2003per@gmail.com>
2026-07-06 10:58:51 +0100

    database connection

---
+++ src/main/java/ajm/spapp/api/controller/UserController.java
@@ -0,0 +1,23 @@
+package ajm.spapp.api.controller;
+
+import ajm.spapp.api.model.User;
+import ajm.spapp.api.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+
+}
